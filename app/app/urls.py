a1a0bc09bce4fd3f98b0dcb1aa75160bc51557b8
Matt Corcoran <mkcorcoran2015@gmail.com>
2026-03-14 15:31:41 +0100

    added user API serializers

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,7 +21,7 @@ from drf_spectacular.views import (
 )
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -31,5 +31,6 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="api-schema"),
         name="api-docs",
     ),
+    path("api/user/", include("user.urls")),
     path("", lambda request: HttpResponse("Home page")),
 ]
