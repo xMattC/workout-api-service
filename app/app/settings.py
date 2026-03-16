525b73cb31dd19fcd475e3211bc4fc984f5d484c
Matt Corcoran <mkcorcoran2015@gmail.com>
2026-03-16 14:40:00 +0100

    Created workout app and added it to INSTALLED_APPS in settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,11 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "core",
-    "user",
     "rest_framework",
     "rest_framework.authtoken",
     "drf_spectacular",
+    "core",
+    "user",
+    "workout",
 ]
 
 MIDDLEWARE = [
