dcf1bcd8f3ecac39cc81c62cb20e03e6b0c01a08
Matt Corcoran <mkcorcoran2015@gmail.com>
2026-03-19 13:42:59 +0100

    fix test + makemigrations

--- app/core/models.py
+++ app/core/models.py
@@ -21,7 +21,7 @@ def workout_image_file_path(instance, filename):
     ext = os.path.splitext(filename)[1]
     filename = f"{uuid.uuid4()}{ext}"
 
-    return os.path.join("uploads", "recipe", filename)
+    return os.path.join("uploads", "workout", filename)
 
 
 # ---------------------------------------------------------------------
