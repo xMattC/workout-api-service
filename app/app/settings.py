3fc634d66b43eda915d0b4701b1814d170082be9
Matt Corcoran <mkcorcoran2015@gmail.com>
2026-03-19 13:24:34 +0100

    Configure project for static files

--- app/app/settings.py
+++ app/app/settings.py
@@ -128,7 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static/static/"
+MEDIA_URL = "/static/media/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
