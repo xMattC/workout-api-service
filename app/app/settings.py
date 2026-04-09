909812628137330b39617c09c1635cf7e04b9294
Matt <73446602+xMattC@users.noreply.github.com>
2026-04-09 14:19:59 +0200

    Deploy Project to AWS (#51)
    * configure files for uwsgi
    
    * Add deployment docker compose config
    
    * update settings for deployment
    
    * Deployment files setup

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,13 +23,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v)dk-**_@4am4pcvaq)d_#_91pbz5nivw^)p9m(!y440o(q&a9"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 # Application definition
 
